60068e6b3ce53bb10e0310518b34f28a9c3ae6c3
Adriangp27 <99752443+Adriangp27@users.noreply.github.com>
2022-04-23 00:25:28 -0400

    Updated Files.

--- Data/schema.sql
+++ Data/schema.sql
@@ -1,3 +1,5 @@
+
+
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
@@ -48,5 +50,13 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+
+CREATE TABLE dept_emp (
+	dept_emp VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
 
